2b9f47aca95c26a09b0663d4ffcddb674d9500b5
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-10-25 21:06:06 +0600

    created all courses route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
-
+const courses = require("./data/courses.json")
+const cors = require("cors")
 const app = express();
+// app.use(cors());
 
 // route
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+// get all courses
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
 
 // listening server
 app.listen("5000", () => {
