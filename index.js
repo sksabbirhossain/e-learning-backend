896c0d96f61325bb1add8f62b110695840cc87bf
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-10-26 03:27:49 +0600

    created courses category route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const courses = require("./data/courses.json")
-const details = require("./data/deatails.json")
-const cors = require("cors")
+const courses = require("./data/courses.json");
+const details = require("./data/deatails.json");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 
@@ -10,16 +10,24 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 // get all courses
-app.get('/api/courses', (req, res) => {
-  res.send(courses)
-})
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
+// get courses by id
 app.get("/api/courses/:id", (req, res) => {
   const courseId = req.params.id;
-  const deatailsCourse = details.filter(items => items.couses_id == courseId)
+  const deatailsCourse = details.filter((items) => items.couses_id == courseId);
   res.send(deatailsCourse);
-  console.log()
-})
+  console.log();
+});
+
+// get details
+app.get("/api/category/:id", (req, res) => {
+  const id = req.params.id;
+  const categoryDetails = details.find((items) => items._id == id);
+  res.send(categoryDetails);
+});
 
 // listening server
 app.listen("5000", () => {
